08b290f5fb830a851195e331e1ffe4cadbbe1688
Greg Fletcher <gnfletcher@gmail.com>
2016-11-16 18:34:44 -0500

    nextOpen() adjustment

--- src/Heap/driver.java
+++ src/Heap/driver.java
@@ -15,10 +15,42 @@ public class driver {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
       TrashHeap heap = new TrashHeap();
-      heap.makeHeap(10);
+      heap.makeHeap(5);
+      heap.print();
       heap.insert(8);
-
-      
+      heap.print();
+      heap.insert(14);
+      heap.printRootKids();
+      heap.print();
+      heap.insert(15);
+      heap.print();
+      heap.insert(16);
+      heap.print();
+      heap.insert(20);
+      heap.print();
+      heap.insert(32);
+      heap.print();
+      heap.insert(36);
+      heap.print();
+      heap.insert(37);
+      heap.print();
+      heap.insert(38);
+      heap.print();
+      heap.insert(39);
+      heap.print();
+      heap.insert(40);
+      heap.print();
+      heap.printNextKids();
+      heap.insert(42);
+      heap.print();
+      heap.insert(100);
+      heap.print();
+      heap.insert(125);
+      heap.print();
+      heap.insert(130);
+      heap.print();
+      heap.insert(150);
+      heap.print();
   }
 
 }
